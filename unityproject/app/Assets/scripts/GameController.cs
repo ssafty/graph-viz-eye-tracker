defa3aa4a72cad0b4767d003d65ca3e473c9ebf1
Mirco <ablx@posteo.de>
2016-08-17 09:40:49 +0200

    The graph objects are now inside a parent object

--- unityproject/app/Assets/scripts/GameController.cs
+++ unityproject/app/Assets/scripts/GameController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Xml;
 using System.IO;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 	public bool loadDataset = false;
 
 	public float SCALE = 2;
@@ -18,51 +19,55 @@ public class GameController : MonoBehaviour {
 	private int edgeCount = 0;
 
 	private TextMesh nodeCountText;
-    private TextMesh edgeCountText;
-    private TextMesh status;
+	private TextMesh edgeCountText;
+	private TextMesh status;
 
-	private IEnumerator loadLayout(){
+	private GameObject graphParent;
+
+
+	private IEnumerator loadLayout ()
+	{
 		string sourceFile;
 
 		if (loadDataset) {
 			sourceFile = "output_2";
-		}else {
+		} else {
 			sourceFile = "sample_output";
 		}
 		status.text = "Loading file: " + sourceFile + ".xml";
 
 		XmlDocument xmlDoc = new XmlDocument ();
-		TextAsset textAsset = (TextAsset)Resources.Load(sourceFile, typeof(TextAsset));
-		xmlDoc.LoadXml(textAsset.text);
+		TextAsset textAsset = (TextAsset)Resources.Load (sourceFile, typeof(TextAsset));
+		xmlDoc.LoadXml (textAsset.text);
 
 		status.text = "Loading ...";
 
 		XmlElement root = xmlDoc.FirstChild as XmlElement;
-        //find most connected element
-        float max_count = find_highest_connection(root);
+		//find most connected element
+		float max_count = find_highest_connection (root);
 
-        //Building nodes and edges
+		//Building nodes and edges
 		for (int i = 0; i < root.ChildNodes.Count; i++) {
 			XmlElement xmlGraph = root.ChildNodes [i] as XmlElement;
 
 			for (int j = 0; j < xmlGraph.ChildNodes.Count; j++) {
 				XmlElement xmlNode = xmlGraph.ChildNodes [j] as XmlElement;
 
-                //create nodes
-                if (xmlNode.Name == "node") {
-                    float x = float.Parse(xmlNode.Attributes["x"].Value) / SCALE;
-                    float y = float.Parse(xmlNode.Attributes["y"].Value) / SCALE;
-                    float z = float.Parse(xmlNode.Attributes["z"].Value) / SCALE;
+				//create nodes
+				if (xmlNode.Name == "node") {
+					float x = float.Parse (xmlNode.Attributes ["x"].Value) / SCALE;
+					float y = float.Parse (xmlNode.Attributes ["y"].Value) / SCALE;
+					float z = float.Parse (xmlNode.Attributes ["z"].Value) / SCALE;
 
-                    Node nodeObject = Instantiate(nodePrefab, new Vector3(x, y, z), Quaternion.identity) as Node;
+					Node nodeObject = Instantiate (nodePrefab, new Vector3 (x, y, z), Quaternion.identity) as Node;
+					nodeObject.transform.parent = graphParent.transform;
+					nodeObject.nodeText.text = WWW.UnEscapeURL (xmlNode.Attributes ["id"].Value.Replace ("_", " "));
+					nodeObject.id = xmlNode.Attributes ["id"].Value;
+					nodes.Add (nodeObject.id, nodeObject);
 
-                    nodeObject.nodeText.text = WWW.UnEscapeURL(xmlNode.Attributes["id"].Value.Replace("_"," "));
-                    nodeObject.id = xmlNode.Attributes["id"].Value;
-                    nodes.Add(nodeObject.id, nodeObject);
-
-                    float count = float.Parse(xmlNode.Attributes["count"].Value);
-                    float size = 0.5f * (count + 1) * SCALE / (max_count + 1);
-                    nodeObject.transform.localScale = new Vector3(size, size, size);
+					float count = float.Parse (xmlNode.Attributes ["count"].Value);
+					float size = 0.5f * (count + 1) * SCALE / (max_count + 1);
+					nodeObject.transform.localScale = new Vector3 (size, size, size);
 
 					status.text = "Loading Topology: Node\n" + nodeObject.id;
 					nodeCount++;
@@ -70,20 +75,20 @@ public class GameController : MonoBehaviour {
 				}
 
 				//create edges
-				if(xmlNode.Name == "edge"){
-					Edge edgeObject = Instantiate(edgePrefab, new Vector3(0,0,0), Quaternion.identity) as Edge;
-					edgeObject.id = xmlNode.Attributes["id"].Value;
-					edgeObject.sourceId = xmlNode.Attributes["source"].Value;
-					edgeObject.targetId = xmlNode.Attributes["target"].Value;
-					edges.Add(edgeObject.id, edgeObject);
-
+				if (xmlNode.Name == "edge") {
+					Edge edgeObject = Instantiate (edgePrefab, new Vector3 (0, 0, 0), Quaternion.identity) as Edge;
+					edgeObject.id = xmlNode.Attributes ["id"].Value;
+					edgeObject.sourceId = xmlNode.Attributes ["source"].Value;
+					edgeObject.targetId = xmlNode.Attributes ["target"].Value;
+					edges.Add (edgeObject.id, edgeObject);
+					edgeObject.transform.parent = graphParent.transform;
 					status.text = "Loading Topology:\nEdge " + edgeObject.id;
 					edgeCount++;
 					edgeCountText.text = "Edges: " + edgeCount;
 				}
 
 				//every 100 cycles return control to unity?? I guess to avoid fps lags
-				if(j % 100 == 0)
+				if (j % 100 == 0)
 					yield return true;
 			}
 		}
@@ -94,49 +99,48 @@ public class GameController : MonoBehaviour {
 
 	}
 
-	private void MapLinkNodes(){
-		foreach(string key in edges.Keys){
-			Edge link = edges[key] as Edge;
-			link.source = nodes[link.sourceId] as Node;
-			link.target = nodes[link.targetId] as Node;
+	private void MapLinkNodes ()
+	{
+		foreach (string key in edges.Keys) {
+			Edge link = edges [key] as Edge;
+			link.source = nodes [link.sourceId] as Node;
+			link.target = nodes [link.targetId] as Node;
 		}
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		nodes = new Hashtable ();
 		edges = new Hashtable ();
 
-		nodeCountText = GameObject.Find ("NodeCountText").GetComponent<TextMesh>();
+		nodeCountText = GameObject.Find ("NodeCountText").GetComponent<TextMesh> ();
 		nodeCountText.text = "Nodes: 0";
-		edgeCountText = GameObject.Find("EdgeCountText").GetComponent<TextMesh>();
+		edgeCountText = GameObject.Find ("EdgeCountText").GetComponent<TextMesh> ();
 		edgeCountText.text = "Edges: 0";
-		status = GameObject.Find("StatusText").GetComponent<TextMesh>();
+		status = GameObject.Find ("StatusText").GetComponent<TextMesh> ();
 		status.text = "";
-
-		StartCoroutine( loadLayout() );
+		graphParent = GameObject.FindGameObjectWithTag ("GraphParent");
+		StartCoroutine (loadLayout ());
 	}
 
-    private float find_highest_connection(XmlElement root)
-    {
-        float max = 0;
-        for (int i = 0; i < root.ChildNodes.Count; i++)
-        {
-            XmlElement xmlGraph = root.ChildNodes[i] as XmlElement;
+	private float find_highest_connection (XmlElement root)
+	{
+		float max = 0;
+		for (int i = 0; i < root.ChildNodes.Count; i++) {
+			XmlElement xmlGraph = root.ChildNodes [i] as XmlElement;
 
-            for (int j = 0; j < xmlGraph.ChildNodes.Count; j++)
-            {
-                XmlElement xmlNode = xmlGraph.ChildNodes[j] as XmlElement;
+			for (int j = 0; j < xmlGraph.ChildNodes.Count; j++) {
+				XmlElement xmlNode = xmlGraph.ChildNodes [j] as XmlElement;
 
-                //create nodes
-                if (xmlNode.Name == "node")
-                {
-                    float count = float.Parse(xmlNode.Attributes["count"].Value);
-                    if (count > max)
-                        max = count;
-                }
-            }
-        }
-        return max;
-    }
+				//create nodes
+				if (xmlNode.Name == "node") {
+					float count = float.Parse (xmlNode.Attributes ["count"].Value);
+					if (count > max)
+						max = count;
+				}
+			}
+		}
+		return max;
+	}
 }
--- unityproject/app/Assets/scripts/Node.cs
+++ unityproject/app/Assets/scripts/Node.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.ComponentModel;
+using System.Reflection;
 
-public class Node : MonoBehaviour {
+public class Node : MonoBehaviour
+{
 
 	public string id;
 	public int count;
 	public TextMesh nodeText;
+	public  string title;
+	public  string desc;
+
+	void Start ()
+	{
+		title = "Node Titel";
+		desc = "Description";
+	}
 	
 	// Update is called once per frame
-	void Update () { 
+	void Update ()
+	{ 
 		Vector3 targetPosition = Camera.main.transform.position;
 		Vector3 currentPosition = transform.position;
 		targetPosition.y = 0;
 		currentPosition.y = 0;
-		nodeText.transform.rotation = Quaternion.LookRotation(currentPosition - targetPosition, Vector3.up);
+		nodeText.transform.rotation = Quaternion.LookRotation (currentPosition - targetPosition, Vector3.up);
 	}
 }
