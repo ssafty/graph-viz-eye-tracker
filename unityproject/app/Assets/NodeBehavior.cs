1ea067d47994673f366a89aac7403303aef7e82c
Taldops <2dobert@informatik.uni-hamburg.de>
2016-08-19 21:25:00 +0200

    FPS Camera implemented. Transperency improved.

--- unityproject/app/Assets/NodeBehavior.cs
+++ unityproject/app/Assets/NodeBehavior.cs
@@ -4,20 +4,23 @@ using System.Collections;
 public class NodeBehavior : MonoBehaviour {
 
 	private GameObject cam;
-	public float opaque_distance = 30.0f;
+	public float opaque_distance;
 
 	private Renderer rend;
+	public float opaqueClamp;
 
 	// Use this for initialization
 	void Start () {
 		cam = GameObject.Find ("Main Camera");
 		rend = GetComponent<Renderer>();
+		opaqueClamp = opaque_distance * transform.lossyScale.magnitude / 2.0f;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		print (transform.lossyScale);
 		float opacity = Vector3.Distance (transform.position, cam.transform.position);
-		opacity = Mathf.Clamp (opacity, 0.0f, opaque_distance) / opaque_distance;
+		opacity = Mathf.Clamp (opacity, 0.0f, opaqueClamp) / opaqueClamp;
 		Color color = new Color (rend.material.color.r, rend.material.color.b, rend.material.color.g, opacity);
 		rend.material.color = color;
 	}
