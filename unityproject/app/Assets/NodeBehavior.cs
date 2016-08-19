43c641585cb6cb781b23032397deaecb5d0e53c0
Taldops <2dobert@informatik.uni-hamburg.de>
2016-08-19 17:13:14 +0200

    Transparency implemented. Nodes become transparent, when the camera gets close.

---
+++ unityproject/app/Assets/NodeBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeBehavior : MonoBehaviour {
+
+	private GameObject cam;
+	public float opaque_distance = 30.0f;
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find ("Main Camera");
+		rend = GetComponent<Renderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float opacity = Vector3.Distance (transform.position, cam.transform.position);
+		opacity = Mathf.Clamp (opacity, 0.0f, opaque_distance) / opaque_distance;
+		Color color = new Color (rend.material.color.r, rend.material.color.b, rend.material.color.g, opacity);
+		rend.material.color = color;
+	}
+}
