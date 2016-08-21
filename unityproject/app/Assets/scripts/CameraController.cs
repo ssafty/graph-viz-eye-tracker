40772b0955bd3c6718fbdb63fbba895447d823b5
Taldops <2dobert@informatik.uni-hamburg.de>
2016-08-21 23:23:59 +0200

    Clean up. Keyboard and mouse also works simultaniously now

--- unityproject/app/Assets/scripts/CameraController.cs
+++ unityproject/app/Assets/scripts/CameraController.cs
@@ -4,28 +4,50 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
 
+	//Keyboard parameters
 	public float zoomSpeed = 3f;
 	public float rotationSpeed = 1.5f;
 	public float strafeSpeed = 0.6f;
+
+	//mouse parameters
+	public float sensitivity;
+	public float scrollSpeed;
+
+
+
+	//Scrolling sets target Position to make it smooth. Is there a better way?
+	private Vector3 targetPos;
+
+	void Start ()
+	{
+		targetPos = transform.position;
+	}	
 
 	void Update ()
 	{
 		//Rotation
 		transform.RotateAround (transform.position, new Vector3 (0, 1.0f, 0), rotationSpeed * Input.GetAxis ("Horizontal"));
+		// X movement
+		transform.RotateAround (transform.position, new Vector3 (0, 1.0f, 0), sensitivity * Input.GetAxis ("Mouse X"));
+
 		//This up/down split is necessary, because the boundaries need to be different to avoid getting stuck because of rounding errors 
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			if (transform.eulerAngles.x > 270 || transform.eulerAngles.x < 85) {
+		if (transform.eulerAngles.x > 270 || transform.eulerAngles.x < 85) {
 				transform.Rotate (new Vector3 (-rotationSpeed * Input.GetAxis ("Vertical"), 0.0f, 0.0f));
-			}
+				transform.Rotate (new Vector3 (-sensitivity * Input.GetAxis ("Mouse Y"), 0.0f, 0.0f));
 		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			if (transform.eulerAngles.x > 275 || transform.eulerAngles.x < 90) {
+		if (transform.eulerAngles.x > 275 || transform.eulerAngles.x < 90) {
 				transform.Rotate (new Vector3 (-rotationSpeed * Input.GetAxis ("Vertical"), 0.0f, 0.0f));
-			}
+				transform.Rotate (new Vector3 (-sensitivity * Input.GetAxis ("Mouse Y"), 0.0f, 0.0f));
 		}
+
 		// Strafing and Zooming
-		transform.Translate (new Vector3 (strafeSpeed * Input.GetAxis ("Horizontal2"), strafeSpeed * Input.GetAxis ("Vertical2"), zoomSpeed * Input.GetAxis ("Zoom")));
-        
+		//transform.Translate (new Vector3 (strafeSpeed * Input.GetAxis ("Horizontal2"), strafeSpeed * Input.GetAxis ("Vertical2"), zoomSpeed * Input.GetAxis ("Zoom")));
+		targetPos = targetPos + transform.forward * (scrollSpeed * Input.GetAxis ("Scroll") + zoomSpeed * Input.GetAxis ("Zoom"))
+			+ transform.up * strafeSpeed * Input.GetAxis ("Vertical2") * 1.5f
+			+ transform.right * strafeSpeed * Input.GetAxis ("Horizontal2") * 1.5f;
+		// transform.Translate can't be used with this smooth scrolling
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime*500);
+
 		//Reset position and angle
 		if (Input.GetKey (KeyCode.R)) {
 			transform.position = new Vector3 (0, 1.0f, -10.0f);
--- unityproject/app/Assets/scripts/Node.cs
+++ unityproject/app/Assets/scripts/Node.cs
@@ -6,19 +6,33 @@ using System.Reflection;
 
 public class Node : MonoBehaviour
 {
-
+	// parts and properties
 	public string id;
 	public int count;
 	public TextMesh nodeText;
 	public  string title = "EMTPY";
 	public  string desc = "EMTPY";
-	private GameObject infoController;
 	public GameObject labelPrefab;
 
+	// Distance at which a Node with size 2 is fully opaque 
+	public float opaque_distance;
+
+	// Scene Camera
+	private GameObject cam;
+
+	// Distance at which this Node is fully opaque
+	private float opaqueClamp;
+
+	private GameObject infoController;
+	private Renderer rend;
 
 	void Start ()
 	{
+		cam = GameObject.Find ("Main Camera");
+		rend = GetComponent<Renderer>();
 
+		// Bigger Nodes become transparent more easily
+		opaqueClamp = opaque_distance * transform.lossyScale.magnitude / 2.0f;
 	}
 	
 	// Update is called once per frame
@@ -29,6 +43,13 @@ public class Node : MonoBehaviour
 		targetPosition.y = 0;
 		currentPosition.y = 0;
 		nodeText.transform.rotation = Quaternion.LookRotation (currentPosition - targetPosition, Vector3.up);
+
+		//Calculate opacity
+		float opacity = Vector3.Distance (transform.position, cam.transform.position);
+		opacity = Mathf.Clamp (opacity, 0.0f, opaqueClamp) / opaqueClamp;
+		//Set opacity
+		Color color = new Color (rend.material.color.r, rend.material.color.b, rend.material.color.g, opacity);
+		rend.material.color = color;
 	}
 
 	public void SetupLabel ()
