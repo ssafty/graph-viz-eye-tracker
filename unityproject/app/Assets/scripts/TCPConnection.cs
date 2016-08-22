4da089c0df015460252b26381b065b109f9a9a22
Mirco,Ahmed,Christian <eyetracker@informatik.uni-hamburg.de>
2016-08-22 13:41:26 +0200

    added tcp script

---
+++ unityproject/app/Assets/scripts/TCPConnection.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Net.Sockets;
+
+public class TCPConnection : MonoBehaviour {
+
+	public string conName = "Localhost";
+	public string conHost = "134.100.10.224";
+	public int conPort = 5006;
+	public bool socketReady = false;
+	
+	TcpClient mySocket;
+	NetworkStream theStream;
+	StreamWriter theWriter;
+	StreamReader theReader;
+	
+	public void setupSocket() {
+		try {
+			mySocket = new TcpClient(conHost, conPort);
+			theStream = mySocket.GetStream();
+			theWriter = new StreamWriter(theStream);
+			theReader = new StreamReader(theStream);
+			socketReady = true;
+		}
+		catch (Exception e) {
+			Debug.Log("Socket error:" + e);
+		}
+	}
+	public void writeSocket(string theLine) {
+		if (!socketReady)
+			return;
+		String tmpString = theLine + "\r\n";
+		theWriter.Write(tmpString);
+		theWriter.Flush();
+	}
+	
+	public string readSocket() {
+		String result = "";
+		if (theStream.DataAvailable) {
+			Byte[] inStream = new Byte[mySocket.SendBufferSize];
+			theStream.Read(inStream, 0, inStream.Length);
+			result += System.Text.Encoding.UTF8.GetString(inStream);
+		}
+		return result;
+	}
+	
+	public void closeSocket() {
+		if (!socketReady)
+			return;
+		theWriter.Close();
+		theReader.Close();
+		mySocket.Close();
+		socketReady = false;
+	}
+
+	public void maintainConnection(){
+		if(!theStream.CanRead) {
+			setupSocket();
+		}
+	}
+
+
+}
