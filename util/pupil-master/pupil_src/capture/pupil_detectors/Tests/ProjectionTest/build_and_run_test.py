e940bb8bba7799ee26ff07fa2c235d1694b8a21b
Saftophobia <asafty2003@gmail.com>
2016-08-22 18:42:53 +0200

    cleaned pupil-lab

--- util/pupil-master/pupil_src/capture/pupil_detectors/Tests/ProjectionTest/build_and_run_test.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == '__main__':
-    import subprocess as sp
-    sp.call("g++ -std=c++11 -I '/usr/local/include/eigen3' -g projectionTest.cpp -o test",shell=True)
-    print "BUILD COMPLETE ______________________"
-    sp.call("./test",shell=True)
