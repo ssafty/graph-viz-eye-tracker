cedd0ad4afab6ec25a5cc9d69aec9228f8ecfedc
Saftophobia <asafty2003@gmail.com>
2016-08-22 12:06:37 +0200

    Added markers to the project

---
+++ unityproject/app/Assets/markers/createMarker.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createMarker : MonoBehaviour {
+
+
+    public int rows, columns;
+    private GameObject[] markers;
+    // Use this for initialization
+    void Start() {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+		Debug.Log (screenWidth + "x" + screenHeight);
+        //screenWidth = 3840; 797
+        //screenHeight = 2160; 393
+        float aspectRatio = screenHeight / screenWidth;
+        float vStepsize = screenHeight / (rows-1);
+        float hStepsize = screenWidth / (columns-1);
+        int lastMarker = 0;
+        markers = GameObject.FindGameObjectsWithTag("marker");
+        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                float x = 0.0f;
+                float y = 0.0f;
+                Vector3 newPos = Vector3.zero;
+
+                if (j == 0 || j == (columns-1) || i == 0 || i == (rows-1))
+                {
+                    x = ((hStepsize * j) - (screenWidth / 2));
+                    y = ((vStepsize * i) - (screenHeight / 2));
+
+					RectTransform rect = markers[lastMarker].GetComponent<RectTransform> ();
+					Vector2 rectPosition = rect.rect.position; //gets x,y
+					//Debug.Log (rectPosition);
+
+                    if (j == 0) { x += markers[lastMarker].transform.localScale.x / 0.9f; }
+                    if (i == 0) { y += markers[lastMarker].transform.localScale.y / 1f; }
+                    if (j == (columns-1))   { x -= markers[lastMarker].transform.localScale.x / 0.9f; }
+                    if (i == (rows-1))      { y -= markers[lastMarker].transform.localScale.y / 1f; }
+
+                    // new screen coordinates should be (USED FOR OFFSET)
+                    // newWidth = (screenWidth)  - (( markers[lastMarker].transform.localScale.x / 0.9f ) * 2) 
+                    // newHeight = -(screenHeight) - (( markers[lastMarker].transform.localScale.y / 1f ) * 2)
+
+                    newPos = new Vector3(x, y, 5);
+					Debug.Log (newPos);
+					markers [lastMarker].GetComponent<RectTransform> ().anchoredPosition = new Vector2 (x, y);
+                    //markers[lastMarker].transform.position = newPos;
+                    lastMarker++;
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
