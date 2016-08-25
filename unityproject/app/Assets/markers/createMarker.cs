7feb6b1176f694eabb7b99512b129d48e159b187
12gabel <2gabel@informatik.uni-hamburg.de>
2016-08-25 17:00:50 +0200

    Mirco/Ahmed NewPupilVersion

--- unityproject/app/Assets/markers/createMarker.cs
+++ unityproject/app/Assets/markers/createMarker.cs
@@ -6,6 +6,10 @@ public class createMarker : MonoBehaviour {
 
     public int rows, columns;
     private GameObject[] markers;
+	public float offset = 1.0f;
+	Vector2 SurfaceBottomLeft = Vector2.zero;
+	Vector2 SurfaceTopRight = Vector2.zero;
+	public Vector2 newScreen;
     // Use this for initialization
     void Start() {
         float screenWidth = Screen.width;
@@ -18,7 +22,6 @@ public class createMarker : MonoBehaviour {
         float hStepsize = screenWidth / (columns-1);
         int lastMarker = 0;
         markers = GameObject.FindGameObjectsWithTag("marker");
-		float offset = 1.3f;
 
         for (int i = 0; i < rows; i++)
         {
@@ -26,7 +29,7 @@ public class createMarker : MonoBehaviour {
             {
                 float x = 0.0f;
                 float y = 0.0f;
-                Vector3 newPos = Vector3.zero;
+                Vector2 newPos = Vector2.zero;
 
                 if (j == 0 || j == (columns-1) || i == 0 || i == (rows-1))
                 {
@@ -34,25 +37,39 @@ public class createMarker : MonoBehaviour {
                     y = ((vStepsize * i) - (screenHeight / 2));
 
 					Rect rect = markers[lastMarker].GetComponent<RectTransform> ().rect;
-					//Debug.Log (rectPosition);
 
 					if (j == 0) { x += rect.width / offset; }
 					if (i == 0) { y += rect.height / offset; }
 					if (j == (columns-1))   { x -= rect.width / offset; }
 					if (i == (rows-1))      { y -= rect.height / offset; }
 
-                    newPos = new Vector3(x, y, 5);
-					Debug.Log (newPos);
+                    newPos = new Vector2(x, y);
 					markers [lastMarker].GetComponent<RectTransform> ().anchoredPosition = new Vector2 (x, y);
-                    //markers[lastMarker].transform.position = newPos;
+
+					if (newPos.x <= SurfaceBottomLeft.x && newPos.y <= SurfaceBottomLeft.y) {
+						SurfaceBottomLeft = newPos; //- new Vector2 (rect.width, rect.height);
+					}
+
+					if (newPos.x >= SurfaceTopRight.x && newPos.y >= SurfaceTopRight.y) {
+						SurfaceTopRight = newPos; //+ new Vector2 (rect.width, rect.height);
+					}
+						
                     lastMarker++;
                 }
             }
         }
-    }
+
+		Rect inner_marker = GameObject.Find ("marker_1").GetComponent<RectTransform> ().rect;
+		SurfaceBottomLeft -= new Vector2 (inner_marker.width/2, inner_marker.height/2);
+		SurfaceTopRight += new Vector2 (inner_marker.width/2, inner_marker.height/2);
+
+		newScreen = new Vector2 (SurfaceTopRight.x - SurfaceBottomLeft.x, SurfaceTopRight.y - SurfaceBottomLeft.y);
+		Debug.Log (newScreen);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
