c87b362a4756c51351ca51f8b25e25fe28bde8fe
Saftophobia <asafty2003@gmail.com>
2016-09-13 19:20:59 +0200

    [Tim] Nodes closer to the camera are relatively opaque

--- unityproject/app/Assets/scripts/Node.cs
+++ unityproject/app/Assets/scripts/Node.cs
@@ -15,10 +15,18 @@ public class Node : MonoBehaviour
 	private GameObject infoController;
 	public GameObject labelPrefab;
 
+	public float opaque_distance; 	// Distance at which a Node with size 2 is fully opaque (currently 12f)
+	private GameObject cam; 		// Scene Camera
+	private float opaqueClamp; 		// Distance at which this Node is fully opaque
+	private Renderer rend;
 
 	void Start ()
 	{
+		cam = GameObject.Find ("Main Camera");
+		rend = GetComponent<Renderer>();
 
+		// Bigger Nodes become transparent more easily
+		opaqueClamp = opaque_distance * transform.lossyScale.magnitude / 2.0f;
 	}
 	
 	// Update is called once per frame
@@ -29,7 +37,12 @@ public class Node : MonoBehaviour
 		targetPosition.y = 0;
 		currentPosition.y = 0;
 		nodeText.transform.rotation = Quaternion.LookRotation (currentPosition - targetPosition, Vector3.up);
+
+		//Calculate opacity
+		float opacity = Vector3.Distance (transform.position, cam.transform.position);
+		opacity = Mathf.Clamp (opacity, 0.0f, opaqueClamp) / opaqueClamp;
+		//Set opacity
+		Color color = new Color (rend.material.color.r, rend.material.color.b, rend.material.color.g, opacity);
+		rend.material.color = color;
 	}
-
-
 }
