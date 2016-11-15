c70f7aa14cfd285af0a737ed3b3d07f771d2da7d
Jannfreiwald <jann.freiwald@googlemail.com>
2016-11-15 15:18:07 +0100

    mirco @ablx /christian: implemented some ways to use the bubble technique

---
+++ unityproject/app/Assets/scripts/Bubble.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubble : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("i got hit");
+    }
+}
