b8b3667fe4e61d90c834beab8cba6698cfb6c93f
Mirco <ablx@posteo.de>
2016-11-29 21:06:03 +0100

    Convinience method to get all highlighted nodes

--- unityproject/app/Assets/scripts/HighlightNode.cs
+++ unityproject/app/Assets/scripts/HighlightNode.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Runtime.ConstrainedExecution;
+using System.Collections.Generic;
 
 public class HighlightNode : MonoBehaviour
 {
 
 	public Color highlightColor = Color.green;
 	private Color white = Color.white;
+	private bool highlighted = false;
 
 	void OnTriggerStay (Collider other)
 	{
@@ -26,6 +28,19 @@ public class HighlightNode : MonoBehaviour
 		bool ok = other.tag == "Bubble";
 		if (ok) {
 			gameObject.GetComponent<Renderer> ().material.color = color;
+			highlighted = gameObject.GetComponent<Renderer> ().material.color == highlightColor;
 		}
+	}
+
+	public static List<GameObject> GetAllHighlighted ()
+	{
+		GameObject[] nodes = GameObject.FindGameObjectsWithTag ("Node");
+		List<GameObject> selected = new List<GameObject> ();
+		foreach (GameObject n in nodes) {
+			if (n.GetComponent<HighlightNode> ().highlighted) {
+				selected.Add (n);
+			}
+		}
+		return selected;
 	}
 }
