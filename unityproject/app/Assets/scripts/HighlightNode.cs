639bfab642415953bee1f4c88605aa2d9b0d3aad
Mirco <ablx@posteo.de>
2016-11-29 16:25:08 +0100

    Color of nodes is now change if they are inside the bubble

---
+++ unityproject/app/Assets/scripts/HighlightNode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.ConstrainedExecution;
+
+public class HighlightNode : MonoBehaviour
+{
+
+	public Color highlightColor = Color.green;
+	private Color white = Color.white;
+
+	void OnTriggerStay (Collider other)
+	{
+		ChangeColorTo (other, highlightColor);
+	}
+
+
+
+	void OnTriggerExit (Collider other)
+	{
+		ChangeColorTo (other, white);
+	}
+
+	private void ChangeColorTo (Collider other, Color color)
+	{
+		Debug.Log (other + "  " + other.tag);
+		bool ok = other.tag == "Bubble";
+		if (ok) {
+			gameObject.GetComponent<Renderer> ().material.color = color;
+		}
+	}
+}
