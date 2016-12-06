be246ecc2c969cda6bb99f0b81760ec1c3d02382
christianpe <0peter@informatik.uni-hamburg.de>
2016-12-06 18:31:48 +0100

    @christianpe working version of switching nodes in a bubble

--- unityproject/app/Assets/scripts/HapringController.cs
+++ unityproject/app/Assets/scripts/HapringController.cs
@@ -16,25 +16,30 @@ public class HapringController : MonoBehaviour {
             nodes = HighlightNode.GetAllHighlighted();
             if(currentIndex == -1 && nodes.Count > 0)
             {
+                Debug.Log("first assigned");
                 currentIndex = nodes[0].GetComponent<Node>().id;
             }
            int index = nodes.FindIndex(c => c.GetComponent<Node>().id == currentIndex);
            Debug.Log("lastIndex =" + index);
-           if (index + 2 > nodes.Count)
+           if (nodes.Count > 0 && index + 2 > nodes.Count)
            {
+                Debug.Log("nodesCount was smaller than 2+index");
                 foreach (GameObject g in nodes)
                 {
                     g.GetComponent<Renderer>().material.color = HighlightNode.highlightColor;
                 }
+                currentIndex = nodes[0].GetComponent<Node>().id;
                 nodes[0].GetComponent<Renderer>().material.color = highlightColor;
            }
-           else
+           else if(nodes.Count > 0)
            {
+                Debug.Log((2+index) +" was not bigger than"+nodes.Count);
                 foreach (GameObject g in nodes)
                 {
                     g.GetComponent<Renderer>().material.color = HighlightNode.highlightColor;
                 }
-                nodes[++index].GetComponent<Renderer>().material.color = highlightColor;
+                currentIndex = nodes[index + 1].GetComponent<Node>().id;
+                nodes[index+1].GetComponent<Renderer>().material.color = highlightColor;
     
            }
         }
