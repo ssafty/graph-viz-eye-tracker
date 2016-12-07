9bb60d9feaaed1034a109c948502be4e906cd629
Savitha <nsavi2@gmail.com>
2016-12-07 23:25:46 +0100

    Revert "@christianpe improved the switching of nodes so you can go in two directions"
    This reverts commit 664c207667daa5447f582046296d2293e61f9f3f.

--- unityproject/app/Assets/scripts/HapringController.cs
+++ unityproject/app/Assets/scripts/HapringController.cs
@@ -10,41 +10,19 @@ public class HapringController : MonoBehaviour {
 	
 	}
 
-    void Update()
-    {
+    void Update () {
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            switchNode(Direction.up);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            switchNode(Direction.down);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            switchNode(Direction.left);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            switchNode(Direction.right);
-        }
-    }
-        
-    void switchNode(Direction direction)
-    {
-        if (direction == Direction.right)
-        {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
             nodes = HighlightNode.GetAllHighlighted();
-            if (currentIndex == -1 && nodes.Count > 0)
+            if(currentIndex == -1 && nodes.Count > 0)
             {
                 Debug.Log("first assigned");
                 currentIndex = nodes[0].GetComponent<Node>().id;
             }
-            int index = nodes.FindIndex(c => c.GetComponent<Node>().id == currentIndex);
-            Debug.Log("lastIndex =" + index);
-            if (nodes.Count > 0 && index + 2 > nodes.Count)
-            {
+           int index = nodes.FindIndex(c => c.GetComponent<Node>().id == currentIndex);
+           Debug.Log("lastIndex =" + index);
+           if (nodes.Count > 0 && index + 2 > nodes.Count)
+           {
                 Debug.Log("nodesCount was smaller than 2+index");
                 foreach (GameObject g in nodes)
                 {
@@ -52,50 +30,19 @@ public class HapringController : MonoBehaviour {
                 }
                 currentIndex = nodes[0].GetComponent<Node>().id;
                 nodes[0].GetComponent<Renderer>().material.color = highlightColor;
-            }
-            else if (nodes.Count > 0)
-            {
-                Debug.Log((2 + index) + " was not bigger than" + nodes.Count);
+           }
+           else if(nodes.Count > 0)
+           {
+                Debug.Log((2+index) +" was not bigger than"+nodes.Count);
                 foreach (GameObject g in nodes)
                 {
                     g.GetComponent<Renderer>().material.color = HighlightNode.highlightColor;
                 }
                 currentIndex = nodes[index + 1].GetComponent<Node>().id;
-                nodes[index + 1].GetComponent<Renderer>().material.color = highlightColor;
-
-            }
+                nodes[index+1].GetComponent<Renderer>().material.color = highlightColor;
+    
+           }
         }
-        else if(direction == Direction.left)
-        {
-            nodes = HighlightNode.GetAllHighlighted();
-            if (currentIndex == -1 && nodes.Count > 0)
-            {
-                Debug.Log("first assigned");
-                currentIndex = nodes[0].GetComponent<Node>().id;
-            }
-            int index = nodes.FindIndex(c => c.GetComponent<Node>().id == currentIndex);
-            Debug.Log("lastIndex =" + index);
-            if (nodes.Count > 0 && (index - 1) >= 0)
-            {
-                foreach (GameObject g in nodes)
-                {
-                    g.GetComponent<Renderer>().material.color = HighlightNode.highlightColor;
-                }
-                currentIndex = nodes[index - 1].GetComponent<Node>().id;
-                nodes[index - 1].GetComponent<Renderer>().material.color = highlightColor;
-            }
-            else if (nodes.Count > 0)
-            {
-                foreach (GameObject g in nodes)
-                {
-                    g.GetComponent<Renderer>().material.color = HighlightNode.highlightColor;
-                }
-                currentIndex = nodes[nodes.Count-1].GetComponent<Node>().id;
-                nodes[nodes.Count-1].GetComponent<Renderer>().material.color = highlightColor;
-
-            }
-        }
-    }
-    enum Direction { up, down, left, right};
+	}
 
 }
