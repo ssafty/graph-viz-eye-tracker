5b149d690cabad9db0d30ba706dfe9837f0288b9
Saftophobia <asafty2003@gmail.com>
2016-12-09 20:50:55 +0100

    Fixed a bug with float division, added seconds

--- util/udp_server_emulator.py
+++ util/udp_server_emulator.py
@@ -1,4 +1,5 @@
 __author__ = 'saftophobia'
+
 import numpy as np
 import socket
 import time
@@ -12,17 +13,12 @@ print "Please press Control+C when you no longer need the emulator running ... "
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def broadcast_frequency(x, y, count_per_second):
-    for i in range(0, count_per_second):
-        x_noised, y_noised = str(x + np.random.normal(0, 0.1)), str(y + np.random.normal(0, 0.1))
-
-        #print "Sending %s,%s"   %(x_noised, y_noised)
+def broadcast_frequency(x, y, seconds_count, count_per_second):
+    for j in range(0, seconds_count * count_per_second):
+        x_noised, y_noised = str(x + np.random.normal(0, 0.008)), str(y + np.random.normal(0, 0.008))
         server.sendto("(%s,%s)" %(x_noised, y_noised), (IP, PORT))
-        time.sleep(1/count_per_second)
+
+        time.sleep(1.0/count_per_second)
 
 while True:
-    x = np.random.random()
-    y = np.random.random()
-
-    broadcast_frequency(x, y, count_per_second= 30)
-
+    broadcast_frequency(np.random.random(), np.random.random(), seconds_count = 6, count_per_second= 30)
