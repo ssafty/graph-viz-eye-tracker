94f659149cb8893bc3b5152d637c52ef17745549
Mirco <ablx@posteo.de>
2017-01-21 13:37:56 +0100

    Fixed the toggle of markers for stereoscopic

--- unityproject/app/Assets/markers/createMarker.cs
+++ unityproject/app/Assets/markers/createMarker.cs
@@ -1,64 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class createMarker : MonoBehaviour {
+public class createMarker : MonoBehaviour
+{
 
 	public float scale;
-    public int rows, columns;
-    private GameObject[] markers;
+	public int rows, columns;
+	private GameObject[] markers;
 	public float offset = 1.0f;
 	Vector2 SurfaceBottomLeft = Vector2.zero;
 	Vector2 SurfaceTopRight = Vector2.zero;
 	public Vector2 newScreen;
 
+
 	private int marker_size;
 	//(100 - scale) * marker_size * 0.5f
 
-    // Use this for initialization
-    void Start() {
+	// Use this for initialization
+	void Start ()
+	{
 		
 	}
 
-	public void update_markers()
+
+	public void update_markers ()
 	{
 		float screenWidth = Screen.width * StereoScript.X_DISTORTION;
-        float screenHeight = Screen.height;
+		float screenHeight = Screen.height;
 		Debug.Log (screenWidth + "x" + screenHeight);
-        //screenWidth = 3840; 797
-        //screenHeight = 2160; 393
-        float aspectRatio = screenHeight / screenWidth;
-        float vStepsize = screenHeight / (rows-1);
-        float hStepsize = screenWidth / (columns-1);
-        int lastMarker = 0;
-        markers = GameObject.FindGameObjectsWithTag("marker");
+		//screenWidth = 3840; 797
+		//screenHeight = 2160; 393
+		float aspectRatio = screenHeight / screenWidth;
+		float vStepsize = screenHeight / (rows - 1);
+		float hStepsize = screenWidth / (columns - 1);
+		int lastMarker = 0;
+		markers = GameObject.FindGameObjectsWithTag ("marker");
 
-        GameObject marker_object = GameObject.Find("markers");
+		GameObject marker_object = GameObject.Find ("markers");
 
-        offset = offset * ((0.5f + scale)/(scale * 0.85f) - 0.8f);
+		offset = offset * ((0.5f + scale) / (scale * 0.85f) - 0.8f);
 
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                float x = 0.0f;
-                float y = 0.0f;
-                Vector2 newPos = Vector2.zero;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				float x = 0.0f;
+				float y = 0.0f;
+				Vector2 newPos = Vector2.zero;
 
-                if (j == 0 || j == (columns-1) || i == 0 || i == (rows-1))
-                {
-                    x = ((hStepsize * j) - (screenWidth / 2));
-                    y = ((vStepsize * i) - (screenHeight / 2));
+				if (j == 0 || j == (columns - 1) || i == 0 || i == (rows - 1)) {
+					x = ((hStepsize * j) - (screenWidth / 2));
+					y = ((vStepsize * i) - (screenHeight / 2));
 
-                    markers [lastMarker].GetComponent<RectTransform> ().localScale = scale * markers [lastMarker].GetComponent<RectTransform> ().localScale;
+					markers [lastMarker].GetComponent<RectTransform> ().localScale = scale * markers [lastMarker].GetComponent<RectTransform> ().localScale;
 
-					Rect rect = markers[lastMarker].GetComponent<RectTransform> ().rect;
+					Rect rect = markers [lastMarker].GetComponent<RectTransform> ().rect;
 
-					if (j == 0) { x += rect.width / offset; }
-					if (i == 0) { y += rect.height / offset; }
-					if (j == (columns-1))   { x -= rect.width / offset; }
-					if (i == (rows-1))      { y -= rect.height / offset; }
+					if (j == 0) {
+						x += rect.width / offset;
+					}
+					if (i == 0) {
+						y += rect.height / offset;
+					}
+					if (j == (columns - 1)) {
+						x -= rect.width / offset;
+					}
+					if (i == (rows - 1)) {
+						y -= rect.height / offset;
+					}
 
-                    newPos = new Vector2(x, y);
+					newPos = new Vector2 (x, y);
 					markers [lastMarker].GetComponent<RectTransform> ().anchoredPosition = new Vector2 (x, y);
 
 					if (newPos.x <= SurfaceBottomLeft.x && newPos.y <= SurfaceBottomLeft.y) {
@@ -69,27 +78,25 @@ public class createMarker : MonoBehaviour {
 						SurfaceTopRight = newPos; //+ new Vector2 (rect.width, rect.height);
 					}
 						
-                    lastMarker++;
-                }
-            }
-        }
+					lastMarker++;
+				}
+			}
+		}
 
-		if(StereoScript.X_DISTORTION != 1)
-		{
-	        for(int i = 0; i < markers.Length; i++)
-	        {
-	        	Vector2 new_pos = markers [i].GetComponent<RectTransform> ().anchoredPosition;
-	        	GameObject duplicate = GameObject.Instantiate(markers [i]);
-	        	duplicate.transform.parent = marker_object.transform;
-	        	Vector2 pos_dup = new_pos + new Vector2(Screen.width * 0.25f, 0);
-	        	new_pos -= new Vector2(Screen.width * 0.25f, 0);
-	        	markers [i].GetComponent<RectTransform> ().anchoredPosition = new_pos;
-	        	duplicate.GetComponent<RectTransform> ().anchoredPosition = pos_dup;
-	        }
+		if (StereoScript.X_DISTORTION != 1) {
+			for (int i = 0; i < markers.Length; i++) {
+				Vector2 new_pos = markers [i].GetComponent<RectTransform> ().anchoredPosition;
+				GameObject duplicate = GameObject.Instantiate (markers [i]);
+				duplicate.transform.parent = marker_object.transform;
+				Vector2 pos_dup = new_pos + new Vector2 (Screen.width * 0.25f, 0);
+				new_pos -= new Vector2 (Screen.width * 0.25f, 0);
+				markers [i].GetComponent<RectTransform> ().anchoredPosition = new_pos;
+				duplicate.GetComponent<RectTransform> ().anchoredPosition = pos_dup;
+			}
 
 			Rect inner_marker = GameObject.Find ("marker_1").GetComponent<RectTransform> ().rect;
-			SurfaceBottomLeft -= new Vector2 (inner_marker.width/2, inner_marker.height/2);
-			SurfaceTopRight += new Vector2 (inner_marker.width/2, inner_marker.height/2);
+			SurfaceBottomLeft -= new Vector2 (inner_marker.width / 2, inner_marker.height / 2);
+			SurfaceTopRight += new Vector2 (inner_marker.width / 2, inner_marker.height / 2);
 
 			newScreen = new Vector2 (SurfaceTopRight.x - SurfaceBottomLeft.x, SurfaceTopRight.y - SurfaceBottomLeft.y);
 			Debug.Log (newScreen);
@@ -98,6 +105,7 @@ public class createMarker : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	}
 }
--- unityproject/app/Assets/scripts/ToggleMarkers.cs
+++ unityproject/app/Assets/scripts/ToggleMarkers.cs
@@ -6,17 +6,24 @@ public class ToggleMarkers : MonoBehaviour
 
 	private GameObject[] markers;
 	private bool state = false;
+	private bool fetch = true;
 	public KeyCode toggleMarkerKey = KeyCode.F12;
 
 	void Start ()
 	{
-		markers = GameObject.FindGameObjectsWithTag ("marker");
+		
 	}
 
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		if (fetch) {
+			markers = GameObject.FindGameObjectsWithTag ("marker");
+			fetch = false;
+		}
+
+
 		if (Input.GetKeyDown (toggleMarkerKey)) {
 			
 			foreach (GameObject m in markers) {
