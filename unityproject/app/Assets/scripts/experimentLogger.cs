fd5bd5ec25657845337ef0ca4ea6ef8844d6d6b7
Mirco <ablx@posteo.de>
2017-01-21 15:48:24 +0100

    Fixed folder path for experiment logger; excluded logs from git

--- unityproject/app/Assets/scripts/experimentLogger.cs
+++ unityproject/app/Assets/scripts/experimentLogger.cs
@@ -1,37 +1,43 @@
 ﻿using UnityEngine;
+
 using System.Collections;
+using System.IO;
 
-public class experimentLogger : MonoBehaviour {
+public class experimentLogger : MonoBehaviour
+{
 
-    System.IO.StreamWriter file;
-    public string currentGraph;
-    public string bubbleSize;
-    public string participantId;
-    public string keypressed;
-    public string trialtime;
-    public string currentHighlightedNode;
-    public string correctNodehit;
-    void Start () {
+	System.IO.StreamWriter file;
+	public string currentGraph;
+	public string bubbleSize;
+	public string participantId;
+	public string keypressed;
+	public string trialtime;
+	public string currentHighlightedNode;
+	public string correctNodehit;
+
+	void Start ()
+	{
         
-        participantId = "1";
-        file = new System.IO.StreamWriter(@"ExperimentLogFolder\"+participantId+".csv");
-        file.WriteLine("timestamp,currentGraph,bubbleSize,participantId,keypressed,currentHighlightedNode,correctNodehit");
-        file.Flush();
-        }
+		participantId = "1";
+		file = new System.IO.StreamWriter (@"ExperimentLogFolder" + Path.DirectorySeparatorChar + participantId + ".csv");
+		file.WriteLine ("timestamp,currentGraph,bubbleSize,participantId,keypressed,currentHighlightedNode,correctNodehit");
+		file.Flush ();
+	}
 
-	void Update()
-    {
-        keypressed = Input.anyKey? Input.inputString : "";
-    }
-	// lateUpdate is called once per frame after all other Updates 
-	void LateUpdate () {
-        file.WriteLine(Time.realtimeSinceStartup + ","+currentGraph + "," + bubbleSize + "," + participantId + "," + keypressed, "," + currentHighlightedNode + "," + correctNodehit);
-        file.Flush();
-        correctNodehit = "";
-    }
+	void Update ()
+	{
+		keypressed = Input.anyKey ? Input.inputString : "";
+	}
+	// lateUpdate is called once per frame after all other Updates
+	void LateUpdate ()
+	{
+		file.WriteLine (Time.realtimeSinceStartup + "," + currentGraph + "," + bubbleSize + "," + participantId + "," + keypressed, "," + currentHighlightedNode + "," + correctNodehit);
+		file.Flush ();
+		correctNodehit = "";
+	}
 
-    public static experimentLogger getLogger()
-    {
-        return GameObject.FindGameObjectWithTag("GameController").GetComponent<experimentLogger>();
-    }
+	public static experimentLogger getLogger ()
+	{
+		return GameObject.FindGameObjectWithTag ("GameController").GetComponent<experimentLogger> ();
+	}
 }
