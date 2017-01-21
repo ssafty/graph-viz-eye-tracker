3f3c015e33e36b643b28d737194b5413f6fae898
Mirco <ablx@posteo.de>
2017-01-21 13:02:18 +0100

    F12 now hides/shows the markers

---
+++ unityproject/app/Assets/scripts/ToggleMarkers.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleMarkers : MonoBehaviour
+{
+
+	private GameObject[] markers;
+	private bool state = false;
+	public KeyCode toggleMarkerKey = KeyCode.F12;
+
+	void Start ()
+	{
+		markers = GameObject.FindGameObjectsWithTag ("marker");
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (toggleMarkerKey)) {
+			
+			foreach (GameObject m in markers) {
+				m.SetActive (state);
+				Debug.Log ("set state to " + state);
+			}
+			state = !state;
+		}
+	
+	}
+}
