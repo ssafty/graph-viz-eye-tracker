329008bf2c2f0bd844d48f58b431ff52c2342264
Mirco <ablx@posteo.de>
2017-02-15 13:04:54 +0100

    After graph chance, a node is highlighted

--- unityproject/app/Assets/scripts/experiment/MoveToExperimentController.cs
+++ unityproject/app/Assets/scripts/experiment/MoveToExperimentController.cs
@@ -3,118 +3,118 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class MoveToExperimentController : ExperimentController {
+public class MoveToExperimentController : ExperimentController
+{
 
-    [SerializeField]
-    Vector3 targetPosition;
-    [SerializeField]
-    int numberOfTrialsForEveryGraph = 1;
-    List<Graph> graphList;
+	[SerializeField]
+	Vector3 targetPosition;
+	[SerializeField]
+	int numberOfTrialsForEveryGraph = 2;
+	List<Graph> graphList;
 
-    // Use this for initialization
-    void Start () {
-        FillTrials();
+	// Use this for initialization
+	void Start ()
+	{
+		FillTrials ();
 	}
 
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
+	// Update is called once per frame
+	protected override void Update ()
+	{
+		base.Update ();
 
-    }
+	}
 
-    protected override void FillTrials()
-    {
-        currentTrials = new List<ExperimentTrial>();
+	protected override void FillTrials ()
+	{
+		Debug.LogWarning ("FillTrials()");
+		currentTrials = new List<ExperimentTrial> ();
+		graphList = new List<Graph> ();
+		graphList.Add (new Graph ("Tree_50", 50, numberOfTrialsForEveryGraph, 5.0f, experimentType.EYE));
+		graphList.Add (new Graph ("Tree_150", 150, numberOfTrialsForEveryGraph, 10.0f, experimentType.MOUSE));
+		//graphList.Add(new Graph("Tree_450",450,5));
+		//graphList.Add(new Graph("200_nodes",200,5));
+		//graphList.Add(new Graph("500_nodes",500,5));
+		//graphList = ShuffleList<Graph> (graphList);
 
-        graphList = new List<Graph>();
-        graphList.Add(new Graph("Tree_50",50,1, 5.0f,experimentType.EYE));
-        graphList.Add(new Graph("Tree_150",150,1, 10.0f, experimentType.MOUSE));
-        //graphList.Add(new Graph("Tree_450",450,5));
-        //graphList.Add(new Graph("200_nodes",200,5));
-        //graphList.Add(new Graph("500_nodes",500,5));
-        graphList = ShuffleList<Graph>(graphList);
+		int k = 0;
+		for (int i = 0; i < (graphList.Count); i++) {
+			k++;
+			CurrentTrials.Add (new MoveToExperimentTrial (k, graphList [i]));
+		}
+	}
 
-        int k = 0;
-        for (int i = 0; i < (graphList.Count); i++)
-        {
-            k++;
-            CurrentTrials.Add(new MoveToExperimentTrial(k, graphList[i]));
-        }
-    }
+	//http://www.vcskicks.com/randomize_array.php
+	private List<E> ShuffleList<E> (List<E> inputList)
+	{
+		List<E> randomList = new List<E> ();
 
-    //http://www.vcskicks.com/randomize_array.php
-    private List<E> ShuffleList<E>(List<E> inputList)
-    {
-        List<E> randomList = new List<E>();
+		System.Random r = new System.Random ();
+		int randomIndex = 0;
+		while (inputList.Count > 0) {
+			randomIndex = r.Next (0, inputList.Count); //Choose a random object in the list
+			randomList.Add (inputList [randomIndex]); //add it to the new, random list
+			inputList.RemoveAt (randomIndex); //remove to avoid duplicates
+		}
 
-        System.Random r = new System.Random();
-        int randomIndex = 0;
-        while (inputList.Count > 0)
-        {
-            randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
-            randomList.Add(inputList[randomIndex]); //add it to the new, random list
-            inputList.RemoveAt(randomIndex); //remove to avoid duplicates
-        }
-
-        return randomList; //return the new random list
-    }
+		return randomList; //return the new random list
+	}
 }
+
 public enum experimentType
 {
-    MOUSE, EYE, HMD
+	MOUSE,
+	EYE,
+	HMD
 }
+
 public class Graph
 {
-    private string _name;
-    private int _numNodes;
-    private int _numberHighlightedNodes;
-    private float _bubbleSize;
-    private experimentType _experimentType;
-    
-    public Graph(string name, int nodes, int numberOfHighlightedNodes, float bubblesize, experimentType type)
-    {
-        _numNodes = nodes;
-        _name = name;
-        _numberHighlightedNodes = numberOfHighlightedNodes;
-        _bubbleSize = bubblesize;
-        _experimentType = type;
-    }
-    public experimentType ExperimentType
-    {
-        get
-        {
-            return _experimentType;
-        }
-    }
-    public string Name
-    {
-        get
-        {
-            return _name;
-        }
-    }
-    public float BubbleSize
-    {
-        get
-        {
-            return _bubbleSize;
-        }
-    }
-    public int NumberHighlightedNodes
-    {
-        get
-        {
-            return _numberHighlightedNodes;
-        }
-    }
-    public int NumNodes
-    {
-        get
-        {
-            return _numNodes;
-        }
-    }
+	private string _name;
+	private int _numNodes;
+	private int _numberHighlightedNodes;
+	private float _bubbleSize;
+	private experimentType _experimentType;
+
+	public Graph (string name, int nodes, int numberOfHighlightedNodes, float bubblesize, experimentType type)
+	{
+		Debug.LogWarning ("Build Graph");
+		_numNodes = nodes;
+		_name = name;
+		_numberHighlightedNodes = numberOfHighlightedNodes;
+		_bubbleSize = bubblesize;
+		_experimentType = type;
+	}
+
+	public experimentType ExperimentType {
+		get {
+			return _experimentType;
+		}
+	}
+
+	public string Name {
+		get {
+			return _name;
+		}
+	}
+
+	public float BubbleSize {
+		get {
+			return _bubbleSize;
+		}
+	}
+
+	public int NumberHighlightedNodes {
+		get {
+			return _numberHighlightedNodes;
+		}
+	}
+
+	public int NumNodes {
+		get {
+			return _numNodes;
+		}
+	}
 }
 
 
--- unityproject/app/Assets/scripts/experiment/MoveToExperimentTrial.cs
+++ unityproject/app/Assets/scripts/experiment/MoveToExperimentTrial.cs
@@ -3,110 +3,99 @@ using System.Collections;
 
 public class MoveToExperimentTrial : ExperimentTrial
 {
-    private Graph _graph;
-    bool first = true;
-    bool currentlyHighlighted = false;
-    int hightlightCounter = 0;
-    bool graphCreated = false;
-    public static Color colorDesAuserwaehlten = new Color(1.0f, 0.0f, 0.0f);
-    public static Color colorDesGefundenenAuserwaehlten = new Color(1.0f, 1.0f, 0.0f);
-    Node selectedNode;
-    public bool done = false;
-    
-    public Graph Graph
-    {
-        get
-        {
-            return _graph;
-        }
-    }
-    public void initialze(GameObject gameController)
-    {
-        if (first)
-        {
-            if (Graph.ExperimentType == experimentType.EYE)
-            {
-                gameController.GetComponent<Bubble>().rayCastAllowed = true;
-            }
-            else if (Graph.ExperimentType == experimentType.MOUSE)
-            {
-                gameController.GetComponent<Bubble>().rayCastAllowed = false;
-            }
-            Bubble.changeBubbleSize(_graph.BubbleSize);
-            first = false;
-            createGraph();
-            experimentLogger.getLogger().currentGraph = _graph.Name;
-            experimentLogger.getLogger().bubbleSize = _graph.BubbleSize.ToString();
-            graphCreated = true;
-            Vector3 pos = Node.GetNodeWithId(0).transform.position;
-            Camera.main.transform.position = new Vector3(pos.x, pos.y, pos.z -20);
-            Camera.main.transform.LookAt(pos);
+	private Graph _graph;
+	bool first = true;
+	int hightlightCounter = 0;
+	bool graphCreated = false;
+	public static Color colorDesAuserwaehlten = new Color (1.0f, 0.0f, 0.0f);
+	public static Color colorDesGefundenenAuserwaehlten = new Color (1.0f, 1.0f, 0.0f);
+	Node nodeToFind;
+	public bool done = false;
 
-        }
+	public Graph Graph {
+		get {
+			return _graph;
+		}
+	}
+
+	public void initialze (GameObject gameController)
+	{
+		
+		if (first) {
+			if (Graph.ExperimentType == experimentType.EYE) {
+				gameController.GetComponent<Bubble> ().rayCastAllowed = true;
+			} else if (Graph.ExperimentType == experimentType.MOUSE) {
+				gameController.GetComponent<Bubble> ().rayCastAllowed = false;
+			}
+			Bubble.changeBubbleSize (_graph.BubbleSize);
+			first = false;
+			createGraph ();
+			highlight ();
+			graphCreated = true;
+
+			Vector3 pos = Node.GetNodeWithId (0).transform.position;
+			Camera.main.transform.position = new Vector3 (pos.x, pos.y, pos.z - 20);
+			Camera.main.transform.LookAt (pos);
+
+			experimentLogger.getLogger ().currentGraph = _graph.Name;
+			experimentLogger.getLogger ().bubbleSize = _graph.BubbleSize.ToString ();
+
+		}
        
-    }
+	}
 
-    public void update()
-    {
-        //set first red node
-        if (!currentlyHighlighted && graphCreated)
-        {
+	private void resetColor ()
+	{
+		if (nodeToFind.GetComponent<Node> ().id != GameObject.FindGameObjectWithTag ("GameController").GetComponent<HapringController> ().currentIndex) {
+			nodeToFind.Highlight (colorDesAuserwaehlten);
+		} else {
+			nodeToFind.Highlight (colorDesGefundenenAuserwaehlten);
+		}
+	}
 
-            if(hightlightCounter < _graph.NumberHighlightedNodes) { 
-                currentlyHighlighted = true;
-                highlight();
-            }
-        }
-        else if(currentlyHighlighted && selectedNode.gotHit)
-        {
-            Debug.Log("Auserwählter = false");
-                selectedNode.derAuserwaehlte = false;
-                Bubble.moveTo(Bubble.REST_POS);
-                selectedNode = null;
-                hightlightCounter++;
+	public void update ()
+	{
+		if (!done) {
+			if (hightlightCounter >= _graph.NumberHighlightedNodes) {
+				done = true;
+			} else {
+				if (nodeToFind.gotHit) {
+					Bubble.moveTo (Bubble.REST_POS);
+					Debug.LogWarning ("got hit");
+					hightlightCounter++;
+					nodeToFind.derAuserwaehlte = false;
+					nodeToFind.gotHit = false;
+					nodeToFind.HighlightDefault ();
+					highlight ();
 
-            if (hightlightCounter < _graph.NumberHighlightedNodes)
-            {
-                highlight();
-            }
+				}
+				resetColor ();
+			}
+		}      
+	}
 
-            else
-            {
-                done = true;
-            }
-            
-        }
-        if(selectedNode != null)
-        {
-            if (selectedNode.GetComponent<Node>().id != GameObject.FindGameObjectWithTag("GameController").GetComponent<HapringController>().currentIndex) {
-                selectedNode.Highlight(colorDesAuserwaehlten);
-            } else
-            {
-                selectedNode.Highlight(colorDesGefundenenAuserwaehlten);
-            }
-            }
-        
-    }
-    private void highlight()
-    {
-        Debug.Log("highlight new Auserwählten");
-        Node node = Node.GetNodeWithId(Random.Range(0, Graph.NumNodes));
-        node.Highlight(colorDesAuserwaehlten);
-        selectedNode = node;
-        node.derAuserwaehlte = true;
-        experimentLogger.getLogger().currentHighlightedNode = node.id_string;
-    }
-    private void createGraph()
-    {
-        GameController gc = GameObject.Find("GameController").GetComponent<GameController>();
-        gc.createGraph(_graph.Name);
-    }
-    /// <summary>
-    /// Constructor
-    /// </summary>
-    /// <param name="_targetPosition"></param>
-    public MoveToExperimentTrial(int trialnum, Graph graph) : base(trialnum)
-    {
-        _graph = graph;
-    }
+	private void highlight ()
+	{
+		Node node = Node.GetNodeWithId (Random.Range (0, Graph.NumNodes));
+		node.Highlight (colorDesAuserwaehlten);
+		node.derAuserwaehlte = true;
+		nodeToFind = node;
+		experimentLogger.getLogger ().currentHighlightedNode = node.id_string;
+	}
+
+	private void createGraph ()
+	{
+		
+		GameController gc = GameObject.Find ("GameController").GetComponent<GameController> ();
+		gc.createGraph (_graph.Name);
+	}
+
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="_targetPosition"></param>
+	public MoveToExperimentTrial (int trialnum, Graph graph) : base (trialnum)
+	{
+		_graph = graph;
+	}
 }
